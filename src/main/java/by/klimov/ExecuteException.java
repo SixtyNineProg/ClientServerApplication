3028ee525ca9f40d9d9dff0c2399d6aa9a6e002a
Dmitry Klimov <dima.klimov.98@inbox.ru>
2023-12-07 19:17:55 +0300

    execute time out test

--- src/main/java/by/klimov/ExecuteException.java
+++ src/main/java/by/klimov/ExecuteException.java
@@ -2,7 +2,7 @@ package by.klimov;
 
 public class ExecuteException extends RuntimeException {
 
-  private static final String DEFAULT_ERROR_MESSAGE = "Execution exception";
+  public static final String DEFAULT_ERROR_MESSAGE = "Execution exception";
 
   public ExecuteException(Exception e) {
     super(DEFAULT_ERROR_MESSAGE, e);
--- src/test/java/by/klimov/ClientTest.java
+++ src/test/java/by/klimov/ClientTest.java
@@ -9,16 +9,31 @@ class ClientTest {
   @Test
   void execute_whenExecute_thenGetValidResponses() {
     // Given
+    int awaitTermination = 20;
     Server server = new Server();
     Client client = new Client(100, server);
     int expected = 5050;
 
     // When
-    boolean end = client.execute(server, 20);
+    boolean end = client.execute(server, awaitTermination);
     int actual = client.getAccumulator();
 
     // Then
     assertThat(actual).isEqualTo(expected);
     assertThat(end).isTrue();
+  }
+
+  @Test
+  void execute_whenExecuteMoreThanAwaitTermination_thenExecuteTimeOut() {
+    // given
+    int awaitTermination = 5;
+    Server server = new Server();
+    Client client = new Client(1000, server);
+
+    // when
+    boolean end = client.execute(server, awaitTermination);
+
+    // then
+    assertThat(end).isFalse();
   }
 }
